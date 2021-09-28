4885f74f74e14ff22017b6780184a15ee625dc24
Miguel Ribeiro <silvaribeiromiguel@gmail.com>
2021-09-28 11:50:12 -0300

    teste conexao heroku

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 // const user = credencias.user;
 // const password = credencias.password;
 // const url = 'mongodb+srv://' + user + ':' + password + '@cluster0.dkgzg.mongodb.net/chatdemo'
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chatdemo', { 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chatdemo', { 
     useNewUrlParse: true,
     useUnifiedTopology: true
 });
