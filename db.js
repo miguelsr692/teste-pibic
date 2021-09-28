40b20f6ab5352e7c961623bc2273c7aca87562b3
Miguel Ribeiro <silvaribeiromiguel@gmail.com>
2021-09-28 11:38:36 -0300

    coment credenciais

--- db.js
+++ db.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/chatdemo');
 
 /* cloud */
-const credencias = require('./credentials');
-const user = credencias.user;
-const password = credencias.password;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://' + user + ':' + password + '@cluster0.dkgzg.mongodb.net/chatdemo', { 
+// const credencias = require('./credentials');
+// const user = credencias.user;
+// const password = credencias.password;
+// const url = 'mongodb+srv://' + user + ':' + password + '@cluster0.dkgzg.mongodb.net/chatdemo'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://UserTest:sesinho10@cluster0.dkgzg.mongodb.net/chatdemo', { 
     useNewUrlParse: true,
     useUnifiedTopology: true
 });
