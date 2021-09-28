d788fd6071531385f2ef0903e2b60c27f62dd6b0
Miguel Ribeiro <silvaribeiromiguel@gmail.com>
2021-09-28 11:35:04 -0300

    conexao atlas com heroku

--- db.js
+++ db.js
@@ -7,7 +7,10 @@ const mongoose = require('mongoose');
 const credencias = require('./credentials');
 const user = credencias.user;
 const password = credencias.password;
-mongoose.connect('mongodb+srv://' + user + ':' + password + '@cluster0.dkgzg.mongodb.net/chatdemo');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://' + user + ':' + password + '@cluster0.dkgzg.mongodb.net/chatdemo', { 
+    useNewUrlParse: true,
+    useUnifiedTopology: true
+});
  
 const medicaoSchema = new mongoose.Schema({
     medicao: {
